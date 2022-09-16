8397482a42409a39c0fb7ad0b87b587a8e8a85c0
ciong <nasheryurag7289@gmal.com>
2022-09-16 15:10:42 +0800

    added addition method

---
+++ addition.java
@@ -0,0 +1,6 @@
+public class addition extends display {
+    void result(int x, int y){
+           int total = x + y;
+           System.out.print("The total addition of two numbers: " + total);
+       }
+   }
